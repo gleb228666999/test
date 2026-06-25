e881b73e3ab0a25115170a6c8840de86e6b69836
gleb228666999 <160999486+gleb228666999@users.noreply.github.com>
2026-06-25 21:56:19 +0300

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -25,7 +25,7 @@ local LocalPlayer = Players.LocalPlayer
 -- ================= ⚙️ НАСТРОЙКИ =================
 local SETTINGS = {
     Enabled = true,
-    MoveSpeed = 20,
+    MoveSpeed = 25,
     CollectionRadius = 4.0,
     LoopDelay = 0.1,
     MaxBagCoins = 40,
