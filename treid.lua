9ec67a70e0bd4cc95446d654a849243289685e58
gleb228666999 <160999486+gleb228666999@users.noreply.github.com>
2026-08-02 14:25:17 +0300

    Update treid.lua

--- treid.lua
+++ treid.lua
@@ -25,7 +25,7 @@ if not AcceptTrade then warn("[ERROR] AcceptTrade не найден!"); return e
 print("[OK] Все Trade remotes найдены!")
 
 -- 🎯 СПИСОК ЦЕЛЕВЫХ АККАУНТОВ
-local TARGETS = {"Finlasd923", "Finasdl20", "Finadkh284", "Finsdhkl203", "Findf3476"}
+local TARGETS = {"Finasdl20", "Finlasd923", "Finsdhkl203", "Finsdhkl203", "Finadkh284"}
 local currentTargetIndex = 1 -- Счетчик для перебора аккаунтов
 
 local MAX_UNIQUE = 4
